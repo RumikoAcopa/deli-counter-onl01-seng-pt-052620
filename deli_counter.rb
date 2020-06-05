0d910a84afa9b2a7874a646b73d7ea4e595679f5
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-05 15:31:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@
 def line(line_array)
   if line_array.size == 0
   puts "The line is currently empty."
-  
+
   else
   name_array = line_array.map.with_index(1) do |name, index|
     "#{index}. #{name}"
