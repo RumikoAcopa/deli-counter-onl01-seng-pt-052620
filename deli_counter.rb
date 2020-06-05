d5b3f449dd05a9f035e78c9b0471608e05374ec3
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-05 14:57:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+#katz_deli = []
+#other_deli = ["Logan", "Avi", "Spencer"]
+#another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(line_array)
+  line_array == 0
+  puts "The line is currently empty."
+
+else
+  name_array = line_array.map.with_index(1) do |name, index|
+    "#{index}. |#{name}"
+end
+  puts "The line is currently: #{name_array.join(' ')}"
